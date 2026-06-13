0bcb66a7a0c1c3baf5dd716cf90bdde5585708ab
Rohini S <rohini8307@gmail.com>
2026-06-13 16:01:38 +0530

    Update Find the Difference.py

--- 389. Find the Difference/Find the Difference.py
+++ 389. Find the Difference/Find the Difference.py
@@ -1,16 +1,8 @@
-char findTheDifference(char* s, char* t) {
-    char c;
-    for(int i=0;t[i]!='\0';i++){
-        int k=0,p=0;
-        for(int j=0;t[j]!='\0';j++){
-            if(t[i]==t[j])
-                k++;
-        }
-        for(int l=0;s[l]!='\0';l++){
-            if(t[i]==s[l])
-            p++;
-        }
-        if(p!=k) c=t[i];    
-    }
-    return c;
-}
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        x = 0
+        for c in s:
+            x ^= ord(c)
+        for c in t:
+            x ^= ord(c)
+        return chr(x)
