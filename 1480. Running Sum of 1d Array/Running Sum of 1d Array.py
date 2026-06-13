f6f07cafd3890f96403c1901c1795595543318f6
Rohini S <rohini8307@gmail.com>
2026-06-13 16:09:08 +0530

    Create Running Sum of 1d Array.py
    Implement a method to calculate the running sum of a 1D array.

---
+++ 1480. Running Sum of 1d Array/Running Sum of 1d Array.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def runningSum(self, nums):
+        a=[0]*len(nums)
+        for i in range(len(nums)):
+            if i==0:
+                a[i]=nums[i]
+                continue
+            a[i]=nums[i]+a[i-1]
+        return a
+
+
+        
